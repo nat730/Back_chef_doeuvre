d6447f736db0679749bacd6f6040a99a1c46d0bb
nat730 <natanaelcolart@gmail.com>
2024-01-19 12:23:42 +0100

    fix-catalogue

--- src/models/catalogue.ts
+++ src/models/catalogue.ts
@@ -2,8 +2,7 @@ import { Sequelize, DataTypes } from "sequelize";
 
 export const CatalogueModel = (sequelize: Sequelize) => {
     return sequelize.define('catalogue', {
-      order_date: DataTypes.DATE,
-      status: DataTypes.STRING,
+      store: DataTypes.STRING,
     });
   };
   
