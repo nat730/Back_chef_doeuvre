9bc6632f4a88aed1a1a0c0f823df23ac57fa8d74
nat730 <natanaelcolart@gmail.com>
2024-01-19 15:40:49 +0100

    add many to many for catalog and product

--- src/index.ts
+++ src/index.ts
@@ -48,8 +48,9 @@ OrderItem.belongsTo(Product, { foreignKey: 'product_id' });
 Category.hasOne(Product, { foreignKey: 'category_id' });
 Product.belongsTo(Category, { foreignKey: 'category_id' });
 
-Catalogue.hasMany(CatalogueItem, { foreignKey: 'catalogue_id' });
-CatalogueItem.belongsTo(Catalogue, { foreignKey: 'catalogue_id' });
+Catalog.belongsToMany(Product, { through: "CatalogItems" });
+Product.belongsToMany(Catalog, { through: "CatalogItems" });
+
 
 //sequelize.sync({ force: true });
 sequelize.sync();
