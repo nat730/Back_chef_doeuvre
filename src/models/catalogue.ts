849b0e61d56efdb3fd1a44ec6ef8f99191989275
nat730 <natanaelcolart@gmail.com>
2024-01-19 15:46:42 +0100

    rebase

--- src/models/catalogue.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize, DataTypes } from "sequelize";
-
-export const CatalogueModel = (sequelize: Sequelize) => {
-    return sequelize.define('catalogue', {
-      store: DataTypes.STRING,
-    });
-  };
-  
