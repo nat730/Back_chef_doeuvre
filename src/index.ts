12ef20373bd842483278b9a43702ae80f359b0da
nat730 <natanaelcolart@gmail.com>
2024-03-12 12:24:25 +0100

    simplification de la bdd

--- src/index.ts
+++ src/index.ts
@@ -22,9 +22,7 @@
   const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
-    dialectOptions: {
-      ssl: process.env.DB_SSLMODE === 'require' ? { require: true, rejectUnauthorized: false } : {}
-    }
+    dialectOptions: { ssl: true, }
   });
   
   
