1fbb7b1e066b81cf880e23b326f75144f82f6c0d
nat730 <natanaelcolart@gmail.com>
2024-03-13 16:54:44 +0100

    modification router pour correspondre au modele de product

--- src/router/Product.ts
+++ src/router/Product.ts
@@ -1,156 +1,102 @@
 import { Request, Response, Router } from "express";
 import { Product, Category } from "..";
-import authenticationMiddleware from "../middleware/middleware_jws";
+import authenticationMiddleware from "../middleware/middleware_connexion";
+import adminMiddleware from "../middleware/middleware_admin";
 
 export const productRouter = Router();
 
 // Create a new product
-productRouter.post("/", async (req: Request, res: Response) => {
+productRouter.post("/", authenticationMiddleware, adminMiddleware, async (req: Request, res: Response) => {
   try {
-    const {
-      name,
-      description,
-      price,
-      priceperkg,
-      priceasso,
-      priceperkgasso,
-      stock_quantity,
-      category_id,
-    } = req.body;
+    const { name, description, unit_value, FKcategory } = req.body;
 
     // Validation des données
-    if (!name || !description || !price || !stock_quantity || !category_id) {
-      return res
-        .status(400)
-        .json({
-          error:
-            "Veuillez fournir toutes les informations nécessaires pour créer un produit.",
-        });
+    if (!name || !description || !unit_value || !FKcategory) {
+      console.error(req.body);
+      return res.status(400).json({
+        error: "Veuillez fournir toutes les informations nécessaires pour créer un produit.",
+      });
     }
 
     // Vérification de l'existence de la catégorie
-    const categoryExists = await Category.findOne({
-      where: { id: category_id },
-    });
+    const categoryExists = await Category.findOne({ where: { name: FKcategory } });
 
     if (!categoryExists) {
-      return res
-        .status(404)
-        .json({ error: "La catégorie spécifiée n'existe pas." });
+      return res.status(404).json({ error: "La catégorie spécifiée n'existe pas." });
     }
 
-    const categoryName = categoryExists.dataValues.name;
+    const category = await Category.findOne({
+      where: { name: FKcategory }
+    });
+
+    if (!category) {
+      return res.status(404).json({ error: "La catégorie spécifiée n'existe pas." });
+    }
 
     // Création d'un nouveau produit
     const newProduct = await Product.create({
       name,
       description,
-      price,
-      priceperkg,
-      priceasso,
-      priceperkgasso,
-      stock_quantity,
-      category_id,
+      unit_value,
+      category_id: category.dataValues.id
     });
 
     // Réponse avec le nouveau produit créé
-    res.status(201).json({ newProduct, categoryName });
+    res.status(201).json({ newProduct, categoryName: categoryExists.dataValues.name });
   } catch (error) {
     console.error("Erreur lors de la création d'un produit :", error);
     res.status(500).json({ error: "Erreur interne du serveur" });
   }
 });
 
+
 // Get all products
-productRouter.get("/", async (req, res) => {
+productRouter.get("/", async (req: Request, res: Response) => {
   try {
-    // Récupérer tous les produits avec les détails de la catégorie
-    const productsWithCategory = await Product.findAll({
-      include: [{ model: Category }],
+    const products = await Product.findAll({
+      include: [{
+        model: Category,
+        attributes: ['name']
+      }]
     });
 
-    // Transformez les données pour remplacer l'ID par le nom de la catégorie
-    const transformedProducts = productsWithCategory.map((Product) => ({
-      id: Product.dataValues.id,
-      name: Product.dataValues.name,
-      description: Product.dataValues.description,
-      price: Product.dataValues.price,
-      priceperkg: Product.dataValues.priceperkg,
-      priceasso: Product.dataValues.priceasso,
-      priceperkgasso: Product.dataValues.priceperkgasso,
-      stock_quantity: Product.dataValues.stock_quantity,
-      categoryName: Product.dataValues.category.name,
-    }));
+    const productsWithCategoryName = products.map(product => {
+      return {
+        ...product.dataValues,
+      };
+    });
+    
 
-    res.json(transformedProducts);
+    res.json(productsWithCategoryName);
   } catch (error) {
     console.error("Erreur lors de la récupération des produits :", error);
     res.status(500).json({ error: "Erreur interne du serveur" });
   }
 });
+
 
 // Get a specific product by ID
 productRouter.get("/:id", async (req: Request, res: Response) => {
   try {
     const productId = req.params.id;
-    const productById = await Product.findByPk(productId);
+    const productById = await Product.findByPk(productId, {
+      include: [{
+        model: Category,
+        attributes: ['name']
+      }]
+    });
 
     if (!productById) {
       return res.status(404).json({ error: "Produit non trouvé." });
     }
 
-    res.json(productById);
+    const productWithCategoryName = {
+      ...productById.dataValues,
+    };
+
+    res.json(productWithCategoryName);
   } catch (error) {
-    console.error("Erreur lors de la récupération des produits :", error);
-    res.status(500).json({ error: "Erreur interne du serveur" });
-  }
-});
-
-// Update a product by ID
-productRouter.put("/:id", async (req: Request, res: Response) => {
-  try {
-    const {
-      name,
-      description,
-      price,
-      priceperkg,
-      priceasso,
-      priceperkgasso,
-      stock_quantity,
-      FKcategory,
-    } = req.body;
-    const productId = req.params.id;
-
-    const productById = await Product.findByPk(productId);
-
-    if (!productById) {
-      return res.status(404).json({ error: "Produit non trouvé." });
-    }
-
-    const categoryExists = await Category.findOne({
-      where: { name: FKcategory },
-    });
-
-    if (!categoryExists) {
-      return res
-        .status(404)
-        .json({ error: "La catégorie spécifiée n'existe pas." });
-    }
-
-    await productById.update({
-      name,
-      description,
-      price,
-      priceperkg,
-      priceasso,
-      priceperkgasso,
-      stock_quantity,
-      FKcategory,
-    });
-
-    res.status(200).json({ message: "Le produit a été modifié" });
-  } catch (error) {
-    console.error("Erreur lors de la modification du produit par ID :", error);
+    console.error("Erreur lors de la récupération du produit :", error);
     res.status(500).json({ error: "Erreur interne du serveur" });
   }
 });
