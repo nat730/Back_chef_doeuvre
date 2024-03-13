1f8eda45ee462156437ae56ffdb4d4f822cc11a2
nat730 <natanaelcolart@gmail.com>
2024-03-13 16:43:48 +0100

    ajout d'un middleware pour identifier les personnes admin

---
+++ src/middleware/middleware_admin.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+
+const adminMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if ((req.customer as any).role === "admin") {
+    next();
+  } else {
+    res.status(403).json({ error: "Accès refusé. Vous devez être un admin."});
+  }
+};
+export default adminMiddleware;
