6b9802a2e8c9f78343a8a931954ecb1502630ebc
nat730 <natanaelcolart@gmail.com>
2024-03-13 16:49:45 +0100

    renomage de firstname et lastname pour correspondre bdd

--- src/models/customer.ts
+++ src/models/customer.ts
@@ -2,8 +2,8 @@ import { DataTypes, Sequelize } from "sequelize";
 
 export const CustomerModel = (sequelize: Sequelize) => {
   return sequelize.define("customer", {
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
+    firstname: DataTypes.STRING,
+    lastname: DataTypes.STRING,
     email: DataTypes.STRING,
     phone: DataTypes.STRING,
     address: DataTypes.STRING,
