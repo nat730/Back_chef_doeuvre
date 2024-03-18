b2b585666c6d7389b13cd887ac61b3e1336ba70a
nat730 <natanaelcolart@gmail.com>
2024-03-18 12:26:21 +0100

    creation crud commande

--- src/router/Order.ts
+++ src/router/Order.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { Order } from "..";
+import { Customer, Order, OrderItem } from "..";
 import authenticationMiddleware from "../middleware/middleware_connexion";
 import adminMiddleware from "../middleware/middleware_admin";
 
@@ -51,3 +51,99 @@ orderRouter.put("/status/:id",adminMiddleware, async (req: Request, res: Respons
         res.status(500).json({ error: "Erreur interne du serveur" });
     }
 });
+
+orderRouter.get("/:firstname", async (req: Request, res: Response) => {
+    try {
+      const { firstname } = req.params;
+  
+      const orders = await Order.findAll({
+        where: {
+          "$customer.firstname$": firstname,
+        },
+        include: [
+          {
+            model: Customer,
+            as: "customer",
+            attributes: [],
+          },
+        ],
+      });
+  
+      if (orders.length === 0) {
+        return res.status(404).json({ error: "Aucune commande trouvée pour ce prénom." });
+      }
+  
+      res.status(200).json(orders);
+    } catch (error) {
+      console.error("Erreur lors de la récupération des commandes", error);
+      res.status(500).json({ error: "Erreur interne du serveur" });
+    }
+  });
+
+  
+  orderRouter.get("/:id", async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+  
+      if (!id) {
+        return res.status(400).json({
+          error: "Veuillez fournir un ID.",
+        });
+      }
+  
+      const order = await Order.findByPk(id);
+  
+      if (!order) {
+        return res.status(404).json({
+          error: "Aucune commande trouvée avec cet ID.",
+        });
+      }
+  
+      res.status(200).json(order);
+    } catch (error) {
+      console.error("Erreur lors de la récupération de la commande", error);
+      res.status(500).json({ error: "Erreur interne du serveur" });
+    }
+  });
+  
+  orderRouter.get("/product/:productId", async (req: Request, res: Response) => {
+    try {
+      const { productId } = req.params;
+  
+      if (!productId) {
+        return res.status(400).json({
+          error: "Veuillez fournir un ID de produit.",
+        });
+      }
+  
+      const orderItems = await OrderItem.findAll({
+        where: {
+          id: productId,
+        },
+        include: [
+          {
+            model: Order,
+            include: [
+              {
+                model: Customer,
+                attributes: ["firstname", "lastname"],
+              },
+            ],
+          },
+        ],
+      });
+  
+      if (orderItems.length === 0) {
+        return res.status(418).json({
+          error: "Aucune commande trouvée avec ce produit.",
+        });
+      }
+  
+      const uniqueOrders = [...new Set(orderItems.map((item) => item.get("Order")))];
+  
+      res.status(200).json(uniqueOrders);
+    } catch (error) {
+      console.error("Erreur lors de la récupération des commandes", error);
+      res.status(500).json({ error: "Erreur interne du serveur" });
+    }
+  }); 
